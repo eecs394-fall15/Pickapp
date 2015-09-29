81a203c1bcb6a7aac73adf25912351b928cc6716
nateyeazel <nateyeazel@gmail.com>
2015-09-29 00:45:13 -0500

    Made the map connect up with the Database

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -1,41 +1,52 @@
 angular.
 	module('example')
 	.controller('GettingStartedController', function($scope, supersonic){
-		
-		
-		// $scope.pickupGameName = "NU pickup Soccer";
-		// var pickupGame = supersonic.data.model('PickupGame');
-		// var object = {
-		// 	Location: '0.0005, 1.0234, 123.00033',
-		// 	Sport: 'Soccer',
-		// 	Time: 1235
-		// };
-		// var game1 = new pickupGame(object);
-		// game1.save().then(function(){
-		// 	PickupGame.findall().then(function(games){
-		// 		$scope.pickupGameName = games[0].Sport;
-		// 	});
-		// });
-
-
+		var gamesData = supersonic.data.model('Game');
 		var map;
-	  $scope.$on('mapInitialized', function(evt, evtMap) {
-	    map = evtMap;
+			$scope.$on('mapInitialized', function(evt, evtMap) {
+			map = evtMap;
+			gamesData.findAll().then(function(games){
+				for (var i =2; i < games.length; i++){
+					currentGame = games[i];
+					var contentString = "<b>" + currentGame.Sport + "</b> <br> " + currentGame.Time;
+					var contentWindow = new google.maps.InfoWindow({
+						content: contentString
+					});
+					var gameLocation = new google.maps.LatLng(parseFloat(currentGame.Lat), parseFloat(currentGame.Lng));
+					var  testMarker= new google.maps.Marker({position: gameLocation, map: map});
+					(function (_contentWindow){
+						testMarker.addListener('click', function(){
+							_contentWindow.open(map, this);
+						});
+					})(contentWindow);
+				}
+				map.panTo(gameLocation);
+			})
+		});
 
-	    var gamesData = supersonic.data.model('Game');
-		var firstGame = {lat: 42.058431, lng: -87.673650};
-		// // gamesData.findall().then(function(games){
-		// // 	firstGame = games[0];
-		// // });
-		var location = {lat: firstGame.lat, lng: firstGame.lng};
-	    // var testMarker = new google.maps.Marker({position: firstGame, map: map});
-	    // map.panTo(e.latLng);
 
-	    // $scope.placeMarker = function(e) {
-	    //   var marker = new google.maps.Marker({position: e.latLng, map: map});
-	    //   map.panTo(e.latLng);
-	    // }
-	  });
-	  
-	  
+		$scope.loadData = function(){
+			var firstGame;
+			var contentWindows = [];
+			gamesData.findAll().then(function(games){
+
+				for (var i =0; i < 2; i++){
+					currentGame = games[i];
+					var contentString = "<b>" + currentGame.Sport + "</b> <br> " + currentGame.Time;
+					var contentWindow = new google.maps.InfoWindow({
+						content: contentString
+					});
+					var gameLocation = new google.maps.LatLng(parseFloat(currentGame.Lat), parseFloat(currentGame.Lng));
+					var  testMarker= new google.maps.Marker({position: gameLocation, map: map});
+					(function (_contentWindow){
+						testMarker.addListener('click', function(){
+							_contentWindow.open(map, this);
+						});
+					})(contentWindow);
+				}
+
+			});
+
+			console.log('working');
+		};
 	});
