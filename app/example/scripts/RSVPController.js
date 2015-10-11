0b6a063b30208ba70acd5923f0738e0086674a71
brendanfrick2016 <brendanfrick2016@u.northwestern.edu>
2015-10-11 16:32:13 -0500

    RSVPController

--- app/example/scripts/RSVPController.js
+++ app/example/scripts/RSVPController.js
@@ -10,4 +10,16 @@ angular.
 		};
 		supersonic.ui.modal.show(modalView, options);
 	};
+
+	$scope.toHome = function()
+	{
+		var modalView = new supersonic.ui.View("example#getting-started");
+		var options = {
+			animate: true
+		};
+		supersonic.ui.modal.show(modalView, options);
+	};
+
+
+
 });
