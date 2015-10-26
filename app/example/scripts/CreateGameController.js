d09b5924cc871c1e99133aff0de7aedbb8deeae0
brendanfrick2016 <brendanfrick2016@u.northwestern.edu>
2015-10-26 10:15:49 -0500

    Create event unstyled, but functional

--- app/example/scripts/CreateGameController.js
+++ app/example/scripts/CreateGameController.js
@@ -46,9 +46,50 @@ angular.
 					var contentString = "<div id='content'> <h2>Create new event</h2>" +
 						"<form novalidate class='simple-form'>" +
 						"   Time: <input type='time' ng-model='game.time' placeholder='HH:mm:ss' required></input><br>" +
-						"  Sport: <input ng-model='game.sport' required></input> <br>" +
-						"    Max: <input ng-model='game.max' required width='40%'></input>Players <br><br>" +
-						"<input type='submit' ng-click='submitNewEvent(game)'></input>" +
+						"   Sport: <select ng-model='game.sport' required>"+
+									"<option value='baseball'>Baseball</option>" +
+									"<option value='basketball'>Basketball</option>" +
+									"<option value='football'>Football</option>" +
+									"<option value='frisbee'>Frisbee</option>" +
+									"<option value='golf'>Golf</option>" +
+									"<option value='soccer'>Soccer</option>" +
+									"<option value='tennis'>Tennis</option>" +
+									"<option value='volleyball'>Volleyball</option>" +
+									"<option value='other'>Other</option>" +
+								 "</select><br>" +
+						"   Max Players: <select ng-model='game.max' required>" + 
+											"<option value=1>1</option>" +
+											"<option value=2>2</option>" +
+											"<option value=3>3</option>" +
+											"<option value=4>4</option>" +
+											"<option value=5>5</option>" +
+											"<option value=6>6</option>" +
+											"<option value=7>7</option>" +
+											"<option value=8>8</option>" +
+											"<option value=9>9</option>" +
+											"<option value=10>10</option>" +
+											"<option value=11>11</option>" +
+											"<option value=12>12</option>" +
+											"<option value=13>13</option>" +
+											"<option value=14>14</option>" +
+											"<option value=15>15</option>" +
+											"<option value=16>16</option>" +
+											"<option value=17>17</option>" +
+											"<option value=18>18</option>" +
+											"<option value=19>19</option>" +
+											"<option value=20>20</option>" +
+											"<option value=21>21</option>" +
+											"<option value=22>22</option>" +
+											"<option value=23>23</option>" +
+											"<option value=24>24</option>" +
+											"<option value=25>25</option>" +
+											"<option value=26>26</option>" +
+											"<option value=27>27</option>" +
+											"<option value=28>28</option>" +
+											"<option value=29>29</option>" +
+											"<option value=30>30</option>" +
+										 "</select><br>" +
+						"<input type='submit' ng-click='submitNewEvent(game)'></input><br><br>" +
 						"</form>";
 					var compiledContent = $compile(contentString)($scope);
 					infowindow = new google.maps.InfoWindow({
