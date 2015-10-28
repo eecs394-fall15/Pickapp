c1d83ca316dac76fecfca7485223cf37cb23f9ba
neha-2-r <rathi@u.northwestern.edu>
2015-10-27 21:40:21 -0500

    Update DrawerController.js

--- app/example/scripts/DrawerController.js
+++ app/example/scripts/DrawerController.js
@@ -4,7 +4,44 @@ angular.
 		$scope.sports = ['Basketball', 'Football', 'Soccer', 'Ultimate Frisbee', 'Baseball', 'Golf', 'Tennis', 'Volleyball'];
 		$scope.sportsFilters = [false, false, false, false, false, false, false, false];
 		$scope.userGames = false;
+		$scope.getURL = function(sport)
+		{
+			var url;
+			sport = sport.toLowerCase();
+			switch(sport) {
+				case "ultimate frisbee":
+				case "frisbee":
+					url = "/icons/frisbee.png";
+					break;
+				case "soccer":
+					url = "/icons/soccer.png";
+					break;
+				case "football":
+					url = "/icons/football.png";
+					break;
+				case "basketball":
+					url = "/icons/basketball.png";
+					break;
+				case "baseball":
+					url = "/icons/baseball.png";
+					break;
+				case "golf":
+					url = "/icons/golf.png";
+					break;
+				case "tennis":
+					url = "/icons/tennis.png";
+					break;
+				case "volleyball":
+					url = "/icons/volleyball.png";
+					break;
+				default:
+					url = "/icons/default.png";
 
+			}
+
+			return url;
+
+		};
 		$scope.submitFilters = function() {
 			var filters = {
 				sports: $scope.sportsFilters,
